a7c4b8aed66db767c1cd40375dd612a5716dba1d
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2022-12-11 04:46:57 +0100

    Add files via upload

---
+++ CourceGame/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CourceGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ExitEmage = new System.Windows.Forms.PictureBox();
+            this.MainLoopTimer = new System.Windows.Forms.Timer(this.components);
+            this.log = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitEmage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExitEmage
+            // 
+            this.ExitEmage.BackColor = System.Drawing.Color.Transparent;
+            this.ExitEmage.BackgroundImage = global::CourceGame.Resource1.ExitBtn;
+            this.ExitEmage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ExitEmage.ErrorImage = null;
+            this.ExitEmage.InitialImage = null;
+            this.ExitEmage.Location = new System.Drawing.Point(774, 3);
+            this.ExitEmage.Name = "ExitEmage";
+            this.ExitEmage.Size = new System.Drawing.Size(23, 22);
+            this.ExitEmage.TabIndex = 0;
+            this.ExitEmage.TabStop = false;
+            this.ExitEmage.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainLoopTimer
+            // 
+            this.MainLoopTimer.Enabled = true;
+            this.MainLoopTimer.Interval = 41;
+            this.MainLoopTimer.Tick += new System.EventHandler(this.MainLoopTimer_Tick);
+            // 
+            // log
+            // 
+            this.log.AutoSize = true;
+            this.log.BackColor = System.Drawing.Color.Lime;
+            this.log.Location = new System.Drawing.Point(13, 13);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(0, 13);
+            this.log.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.ExitEmage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.ExitEmage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ExitEmage;
+        private System.Windows.Forms.Timer MainLoopTimer;
+        private System.Windows.Forms.Label log;
+    }
+}
+
---
+++ CourceGame/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CourceGame
+{
+    public partial class Form1 : Form
+    {
+        int[] _map;
+        int _viewX = 0;
+
+        Image _groundBlock = Resource1.Ground1;
+        Image _sendBlock = Resource1.Ground2;
+
+        const int blockHeight = 80, blockWidth = 80;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.DoubleBuffer, true );
+
+            Random r = new Random();
+            _map = new int[30];
+
+            for (int i = 0; i < _map.Length; i++)
+            {
+                _map[i] = r.Next(1, 3);
+            }
+        }
+        private void MainLoopTimer_Tick(object sender, EventArgs e)
+        {
+            Point cursor = PointToClient(Cursor.Position);
+
+            float deep = 0; // ... 50
+            float borderRadius = 50;
+            float speed;
+
+            if (cursor.X > 750)
+            {
+                deep = 50 - (Width - cursor.X);  
+            }
+            else if (cursor.X < 50)
+            {
+                deep = -50 + cursor.X;
+            }
+
+            deep = Clamp((int)deep, -50, 50);
+
+            speed = 20 * (deep / borderRadius);
+
+            _viewX += (int)speed;
+            _viewX = Clamp(_viewX, 0, _map.Length * blockWidth - 800);
+
+            log.Text = cursor.ToString();
+
+            Invalidate(); // redrawing scene
+            Refresh();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for(int i = 0; i < _map.Length; i++)
+            {
+                Image currentImage = null;
+                if (_map[i] == 1)
+                {
+                    currentImage = _groundBlock;
+                }
+                else if ( _map[i] == 2)
+                {
+                    currentImage = _sendBlock;
+                }
+
+                g.DrawImage(currentImage, new Rectangle((i * blockWidth) - _viewX, 520, blockWidth, blockHeight));
+
+            } 
+        }
+        private int Clamp(int value, int min, int max)
+        {
+            if (value > max)
+            {
+                return max;
+            }
+            else if (value < min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
