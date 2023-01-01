82639f912c4011af1698eebed18c9e89aebf2633
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2023-01-02 00:53:30 +0100

    Add files via upload

---
+++ CourceGame/WorldReview/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WorldReview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenWorld
+            // 
+            this.btnOpenWorld.Location = new System.Drawing.Point(13, 13);
+            this.btnOpenWorld.Name = "btnOpenWorld";
+            this.btnOpenWorld.Size = new System.Drawing.Size(104, 23);
+            this.btnOpenWorld.TabIndex = 0;
+            this.btnOpenWorld.Text = "Open World";
+            this.btnOpenWorld.UseVisualStyleBackColor = true;
+            this.btnOpenWorld.Click += new System.EventHandler(this.btnOpenWorld_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenWorld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenWorld;
+    }
+}
+
---
+++ CourceGame/WorldReview/Form1.cs
@@ -0,0 +1,49 @@
+﻿using GameCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldReview
+{
+    public partial class Form1 : Form
+    {
+        int[,] _map;
+        public Form1()
+        {
+            InitializeComponent();
+            _map = new int[0,0];
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Red);
+            for(int x = 0; x < _map.GetLength(0); x++)
+            {
+                for (int y = 0; y < _map.GetLength(1); y++)
+                {
+                    if(_map[x, y] != 0)
+                        g.DrawRectangle(p, new Rectangle(x * 10, y * 10, 10, 10));
+                }
+            }
+        }
+
+        private void btnOpenWorld_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _map = Core.LoadWorld(openFileDialog1.FileName);
+            }
+
+            Invalidate();
+            Refresh();
+        }
+    }
+}
