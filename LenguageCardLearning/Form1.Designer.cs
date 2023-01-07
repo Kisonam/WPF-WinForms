230dea46d6f1889a2f254d2d347b3fc5c264c8a3
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2023-01-07 22:58:42 +0100

    Add files via upload

--- LenguageCardLearning/Form1.Designer.cs
+++ LenguageCardLearning/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CardApplication
+﻿namespace LenguageCardLearning
 {
     partial class Form1
     {
@@ -29,54 +29,131 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Work = new System.Windows.Forms.GroupBox();
+            this.txtTranslate = new System.Windows.Forms.TextBox();
+            this.txtTrans = new System.Windows.Forms.TextBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSaveWords = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.Work.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // Work
             // 
-            this.button1.AllowDrop = true;
-            this.button1.Location = new System.Drawing.Point(901, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Work.Controls.Add(this.txtTranslate);
+            this.Work.Controls.Add(this.txtTrans);
+            this.Work.Controls.Add(this.txtWord);
+            this.Work.Controls.Add(this.label3);
+            this.Work.Controls.Add(this.label2);
+            this.Work.Controls.Add(this.label1);
+            this.Work.Location = new System.Drawing.Point(12, 12);
+            this.Work.Name = "Work";
+            this.Work.Size = new System.Drawing.Size(1043, 125);
+            this.Work.TabIndex = 1;
+            this.Work.TabStop = false;
+            this.Work.Text = "workPlace";
             // 
-            // button2
+            // txtTranslate
             // 
-            this.button2.Location = new System.Drawing.Point(487, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.txtTranslate.Location = new System.Drawing.Point(800, 63);
+            this.txtTranslate.Name = "txtTranslate";
+            this.txtTranslate.Size = new System.Drawing.Size(223, 20);
+            this.txtTranslate.TabIndex = 5;
+            // 
+            // txtTrans
+            // 
+            this.txtTrans.Location = new System.Drawing.Point(389, 63);
+            this.txtTrans.Name = "txtTrans";
+            this.txtTrans.Size = new System.Drawing.Size(225, 20);
+            this.txtTrans.TabIndex = 4;
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(21, 63);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(209, 20);
+            this.txtWord.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(886, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Translate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(470, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Transcription";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "English";
+            // 
+            // btnSaveWords
+            // 
+            this.btnSaveWords.Location = new System.Drawing.Point(1062, 60);
+            this.btnSaveWords.Name = "btnSaveWords";
+            this.btnSaveWords.Size = new System.Drawing.Size(158, 35);
+            this.btnSaveWords.TabIndex = 2;
+            this.btnSaveWords.Text = "Save Word";
+            this.btnSaveWords.UseVisualStyleBackColor = true;
+            this.btnSaveWords.Click += new System.EventHandler(this.btnSaveWords_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(1062, 19);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(158, 35);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create file";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1038, 590);
-            this.ControlBox = false;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1232, 761);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnSaveWords);
+            this.Controls.Add(this.Work);
             this.DoubleBuffered = true;
-            this.Enabled = false;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.RightToLeftLayout = true;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "EnglishCard";
+            this.Text = "Learning";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Work.ResumeLayout(false);
+            this.Work.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox Work;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTranslate;
+        private System.Windows.Forms.TextBox txtTrans;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Button btnSaveWords;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
--- LenguageCardLearning/Form1.cs
+++ LenguageCardLearning/Form1.cs
@@ -1,30 +1,57 @@
-﻿using System;
+﻿using CsvHelper;
+using DocumentFormat.OpenXml.Bibliography;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
 using System.Windows.Forms;
 
-namespace CardApplication
+namespace LenguageCardLearning
 {
     public partial class Form1 : Form
     {
+
+        ExcelHandler handler = new ExcelHandler();
+        CsvHandler CsvHandler = new CsvHandler();
+        string filePath = "D:\\Data.csv";
         public Form1()
         {
             InitializeComponent();
-        }
 
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnSaveWords_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            CsvHandler.AddInBase(filePath, txtWord.Text, txtTrans.Text, txtTranslate.Text);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            handler.NewFile();
+            handler.NewSheet();
+            handler.SaveAs(filePath);
+            handler.CloseExcel();
         }
     }
 }
+//var records = new List<CsvHandler>
+//{
+//    new CsvHandler {Word = txtWord.Text, Transcription = txtTrans.Text, Translate = txtTranslate.Text}
+//};
+
+//using (var writer = new StreamWriter(filePath))
+//using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+//{
+
+//    csv.WriteHeader<CsvHandler>();
+//    csv.NextRecord();
+//    foreach (var record in records)
+//    {
+//        csv.NextRecord();
+//        csv.WriteRecord(record);
+
+//    }
+//}
